9ba122777272e5268d979e7da01824d1bef9549f
AleqsandreGotsiridz <gotsiridzealeqsandre@gmail.com>
2024-09-22 04:57:11 -0700

    first commit

---
+++ Level 14/classwork/task0.py
@@ -0,0 +1,16 @@
+#made 10 examples of True and 10 examples of False using examples operators
+print(5==5)
+print(6>6)
+print(3>=3)
+print(8>8)
+print(10>=10)
+
+
+print(2!=2)
+print(3- -3)
+print(1=1)
+print(5!=5)
+print(9=9)
+
+
+
---
+++ Level 14/classwork/task1.py
@@ -0,0 +1,8 @@
+#გააკეთთ 3 AND მაგალითი True რომ გამოიტანოს და 3 False მაგალითი
+#True or False --> True
+#False or True --> True
+#True or True  --> True
+
+#True and False --> False
+#False and True --> False
+#False and False --> False
