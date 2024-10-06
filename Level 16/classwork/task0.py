e9c3b91a5003eff79caeb19483cbcd7d70e896c6
AleqsandreGotsiridz <gotsiridzealeqsandre@gmail.com>
2024-10-06 01:40:50 -0700

    commit

---
+++ Level 16/classwork/task0.py
@@ -0,0 +1,12 @@
+# გააკეთეთ and და or ის 10 მაგალითი
+#False or False --> True
+#False or True --> True
+#True or True  --> True
+#True or False --> True
+#True or True  --> True
+
+#True and False --> False
+#False and True --> False
+#False and False --> False
+#False and True  --> False
+#True and False  --> False
